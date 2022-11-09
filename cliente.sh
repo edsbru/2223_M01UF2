7a01a17fb98ad056c82f842e4285a19bf748eb6a
edsbru <eduardo.salazar@enti.cat>
2022-11-09 11:12:13 +0100

    Comprobaciones y vacas

--- cliente.sh
+++ cliente.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+PORT="2223"
+SERVER_AD="localhost"
+
 echo "Cliente TURIP"
 
-echo "(1) SEND_MSG: HOLI_TURIP"
-echo "HOLI_TURIP 127.0.0.1" | nc localhost 2223
+echo "(1) SEND: Handshake HOLI_TURIP"
+
+echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_AD $PORT
+
+echo "(2) LISTEN: Comprobación Handshake"
+
+MSG=`nc -l $PORT`
+
+echo $MSG
+
+if [ "$MSG" != "OK_TURIP" ]
+then
+	echo "ERROR 1: Handshake incorrecto"
+	exit 1
+fi
+
+echo "(5) SEND: Nombre del archivo"
+
+echo "FILE_NAME vaca.vaca" | nc $SERVER_AD $PORT
+
+echo "(6) LISTEN: Comprobación nombre del archivo"
+
+MSG=`nc -l $PORT`
+
+
+
+
+exit 0
